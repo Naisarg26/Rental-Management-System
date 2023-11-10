eacfbbf17cd6693e77f00027037c9b2c29886044
Modi, Naisarg Vinaybhai <nvmodi@iu.edu>
2023-11-10 00:57:12 -0500

    Update FeaturedProperties.jsx

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -4,11 +4,16 @@ import "./featuredProperties.css";
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
+  // Check if 'data' is an array before attempting to map over it
+  const isValidDataArray = Array.isArray(data);
+
   return (
     <div className="fp">
       {loading ? (
         "Loading"
-      ) : (
+      ) : error ? ( // Check if there is an error and display a message
+        <p>Error loading featured properties.</p>
+      ) : isValidDataArray ? ( // If 'data' is an array, render the list
         <>
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
@@ -20,13 +25,17 @@ const FeaturedProperties = () => {
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
             </div>
           ))}
         </>
+      ) : ( // If 'data' is not an array, render a suitable fallback or message
+        <p>No featured properties available.</p>
       )}
     </div>
   );
